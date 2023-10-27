3bc1e6e8be41f5ba322c9be7c1372e0f9bb5754b
gevorah <jhon.ijaji@encora.com>
2023-10-27 14:58:42 -0500

    fix: cleaning code

--- src/main/java/com/encora/proaatopic/configs/AppConfig.java
+++ src/main/java/com/encora/proaatopic/configs/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
